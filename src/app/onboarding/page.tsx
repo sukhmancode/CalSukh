1ee3d95a3d719d5f7d91807c50889ddde2d550d5
sukhmancode <coder4614@gmail.com>
2025-03-09 02:38:09 +0530

    added

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -58,7 +58,7 @@ const OnboardingPage = () => {
 
               <div className="flex rounded-md">
                 <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-muted-foreground text-sm">
-                  CalMarshal.com/
+                  CalSukh.com/
                 </span>
                 <Input
                   type="text"
