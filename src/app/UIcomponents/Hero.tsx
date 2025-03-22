62590c7a28c79338b31abd84a8c8ff1a7ec9f5fb
sukhmancode <coder4614@gmail.com>
2025-03-22 13:31:23 +0530

    added

--- src/app/UIcomponents/Hero.tsx
+++ src/app/UIcomponents/Hero.tsx
@@ -4,9 +4,9 @@ import { Calendar } from '../components/Calender';
 
 export const Hero = () => {
   return (
-    <div className="bg-secondary p-5 md:p-10 rounded-md shadow-xl flex flex-col md:flex-row gap-5 mt-10 items-center ">
+    <div className="bg-secondary p-5 md:p-10 rounded-md shadow-xl flex flex-col md:flex-row gap-5 mt-10 items-center justify-between ">
       <div className="flex flex-col gap-5 w-full md:w-1/2  p-5">
-        <div className="bg-primary rounded-md w-fit px-5 py-1 flex items-center gap-2">
+        <div className="bg-primary text-white rounded-md w-fit px-5 py-1 flex items-center gap-2">
             CalSukh.com launches v4.9 <ArrowBigRightIcon size={16}/>
         </div>
         <div className="flex flex-col gap-5">
@@ -23,7 +23,7 @@ export const Hero = () => {
           <AuthButtons />
         </div>
       </div>
-      <div className=" w-full flex md:w-1/2 justify-end border rounded-md border-primary ">
+      <div className=" w-full flex md:w-fit p-4 justify-end border rounded-md border-primary ">
         <Calendar />
       </div>
     </div>
